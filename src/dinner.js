dc7efd101e8f5fa6a240beb57acebdaa5bc01816
sooman-hub <65305801+sooman-hub@users.noreply.github.com>
2020-06-28 15:00:23 +0500

    add file dinner.js

---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dinner (props){
+    return(
+        <div>
+            <h1>Today we are serving  chicken Biryani</h1>
+            <h1>Today we are serving chicken Karahi</h1>
+        </div>
+    )
+}
+
+export default Dinner;
